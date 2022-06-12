d4d92d23e82018ea4ffd513cae2d1e31f10f37cb
MarcoFernandez14 <101527736+MarcoFernandez14@users.noreply.github.com>
2022-06-12 15:55:47 -0700

    Adding table final code

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,25 @@ function buildTable(data) {
         });
     });
 }
+
+//Add filters
+function handleClick() {
+    //Grab the datetime value from the filter //D3 to look for the #datetime id in the HTML tags 
+    let date = d3.select("#datetime").property("value");
+    //Set a default filter
+    let filteredData = tableData;
+    //Check to see if a date was entered and filter the data using that date
+    if (date) {
+        //Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //Rebuild the table using the filtered data
+    //@NOTE: If no date was entered, then filteredData will just be the original tableData.
+    buildTable(filteredData);
+}
+
+//Listen for the event //#filter-btn: selector string that contains the id for another HTML tag (button)
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
